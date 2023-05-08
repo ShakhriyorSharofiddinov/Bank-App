58398f46de177ce9475aebc124d8c4bd3187db46
Shakhriyor <shaxriyorsharofiddinov@gmail.com>
2023-05-08 08:18:37 +0500

    first commit

---
+++ android/app/src/main/kotlin/uz/sharofiddinov/bank_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.sharofiddinov.bank_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'home_widget.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeWidget(),
+    );
+  }
+}
